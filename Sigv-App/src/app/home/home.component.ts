4a6e89cdcdc1449fd96370b15ee42833a03cb9cb
fonseca <altilianofonseca@gmail.com>
2021-01-03 01:26:29 +0000

    add register

--- Sigv-App/src/app/home/home.component.ts
+++ Sigv-App/src/app/home/home.component.ts
@@ -8,12 +8,23 @@ import { FormGroup, FormBuilder, RequiredValidator, Validators } from '@angular/
 })
 export class HomeComponent implements OnInit {
   loginForm: FormGroup;
+  registerForm: FormGroup;
+  isLoginForm: boolean = false;
+  isRegisterForm: boolean = false;
 
   constructor(private fb: FormBuilder) {
     this.loginForm = this.fb.group({
       email: ['' , Validators.required],
       password: ['', [Validators.required, Validators.minLength(4)]]
-    });
+    }),
+    this.registerForm = this.fb.group({
+      email: ['' , Validators.required],
+      password: ['', [Validators.required, Validators.minLength(4)]],
+      confirmEmail: ['' , Validators.required],
+      dateOfBithDay: ['' , Validators.required],
+      confirmPassword: ['', [Validators.required, Validators.minLength(4)]],
+    }, {validators: this.passwordMatchValidator, Validators: this.emailMatchValidator});
+
   }
 
   ngOnInit() {
@@ -24,6 +35,44 @@ export class HomeComponent implements OnInit {
   login() {
   if  (this.loginForm.valid)  {}
 
+  }
+
+  goToLoginForm() {
+    this.isLoginForm = true;
+    this.isRegisterForm = false;
+    this.refreshForm();
+  }
+
+  gotoRegisterForm() {
+    this.isLoginForm = false;
+    this.isRegisterForm = true;
+    this.refreshForm();
+  }
+
+  getForm()  {
+    if (this.isLoginForm) {
+      return this.loginForm;
+    }
+      return this.registerForm;
+  }
+
+  commumSubmit() {
+    if  ( this.isLoginForm) {
+      this.login();
+    }
+  }
+
+  refreshForm() {
+    this.getForm();
+  }
+
+  passwordMatchValidator(g: FormGroup)  {
+    return g.get('password')?.value === g.get('confirmPassword')?.value ?  null : { 'missmatch': true};
+  }
+
+
+  emailMatchValidator(g: FormGroup)  {
+    return g.get('email')?.value === g.get('confirmEmail')?.value ?  null : { 'missmatch': true};
   }
 
 }
